c46533ce4a403bf6455b14320bf30c8dc1a413e2
Pierre Bertet <hi@bpier.re>
2025-03-25 22:24:35 +0000

    React demo update
    - Use explicit extensions for imports.
    - Upgrade dependencies (react & vite).
    - Target es2021 (same as blo).
    - Move from file: to link: for blo.

--- demos/react/vite.config.ts
+++ demos/react/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
   build: {
-    target: ["es2020", "esnext"],
+    target: ["es2021", "esnext"],
     outDir: "dist",
     sourcemap: mode === "production" || "inline",
   },
